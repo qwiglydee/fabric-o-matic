6d9eb6ff35fbf6819e6bb4a2c3c4be90a94b23eb
QwiglyDee <qwiglydee@gmail.com>
2019-11-28 04:56:59 +0700

    inserting templates in current material

--- addon/__init__.py
+++ addon/__init__.py
@@ -66,7 +66,6 @@ class AddMenu(MenuBase, bpy.types.Menu):
         layout = self.layout
         layout.menu(AddUtilMenu.bl_idname)
         layout.menu(AddNodeMenu.bl_idname)
-        layout.separator()
         for k in templates.AddTemplateOp.TEMPLATES.keys():
             op = layout.operator(templates.AddTemplateOp.bl_idname, text=k)
             op.template = k
